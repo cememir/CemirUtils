0672cb5d6bc900a3b18cbacdf300aa7620b2ff53
cem emir <cememir2017@gmail.com>
2024-05-25 15:16:46 +0300

    yeni fonksiyonlar eklendi

--- cemirutils/utils.py
+++ cemirutils/utils.py
@@ -1,11 +1,75 @@
 class CemirUtils:
     def __init__(self, data):
         """
-        Veriyi başlatır.
-        Args:
-            data (list): İşlenecek veri listesi.
+        CemirUtils sınıfının yapıcı fonksiyonu.
+        Verilen veriyi sınıfın 'data' değişkenine atar ve sadece int ve float olanları filtreler.
+
+        Parametre:
+        data (list): İşlenecek veri listesi.
         """
-        self.data = data
+        self.data = [x for x in data if isinstance(x, (int, float))]
+
+    def filter_greater_than(self, threshold):
+        """
+        Belirtilen eşik değerinden büyük olan öğeleri filtreler.
+
+        Parametre:
+        threshold (int/float): Eşik değer.
+
+        Dönüş:
+        list: Eşik değerinden büyük olan öğeleri içeren liste.
+
+        Örnek:
+        >>> cem = CemirUtils([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5])
+        >>> cem.filter_greater_than(5)
+        [9, 6]
+        """
+        return [x for x in self.data if x > threshold]
+
+    def filter_less_than(self, threshold):
+        """
+        Belirtilen eşik değerinden küçük olan öğeleri filtreler.
+
+        Parametre:
+        threshold (int/float): Eşik değer.
+
+        Dönüş:
+        list: Eşik değerinden küçük olan öğeleri içeren liste.
+
+        Örnek:
+        >>> cem = CemirUtils([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5])
+        >>> cem.filter_less_than(4)
+        [3, 1, 1, 2, 3]
+        """
+        return [x for x in self.data if x < threshold]
+
+    def sum_values(self):
+        """
+        Listedeki tüm sayısal değerlerin toplamını hesaplar.
+
+        Dönüş:
+        int/float: Listedeki sayısal değerlerin toplamı.
+
+        Örnek:
+        >>> cem = CemirUtils([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5])
+        >>> cem.sum_values()
+        44
+        """
+        return sum(self.data)
+
+    def average(self):
+        """
+        Listedeki sayısal değerlerin ortalamasını hesaplar.
+
+        Dönüş:
+        float: Listedeki sayısal değerlerin ortalaması. Liste boşsa 0 döner.
+
+        Örnek:
+        >>> cem = CemirUtils([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5])
+        >>> cem.average()
+        4.0
+        """
+        return sum(self.data) / len(self.data) if self.data else 0
 
     def head(self, n=5):
         """
--- testet.py
+++ testet.py
@@ -1,11 +1,10 @@
 from cemirutils import CemirUtils
 
-data_list = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+data_list = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, "a", "b", "c"]
 cem = CemirUtils(data_list)
-print(data_list)
-print(cem.head(2))  # Listenin ilk 5 elemanını yazdırır.
-print(cem.tail(4))  # Listenin son 5 elemanını yazdırır.
-print(cem.main())  # Listenin ortadaki elemanlarını yazdırır.
-print(cem.unique_values())  # Listenin benzersiz elemanlarını yazdırır.
-print(cem.sort_asc())  # Listenin artan sırada sıralanmış halini yazdırır.
-print(cem.sort_desc())  # Listenin azalan sırada sıralanmış halini yazdırır.
+
+print(data_list)  # Orijinal veri listesini yazdırır
+print(cem.filter_greater_than(5))  # 5'ten büyük değerleri yazdırır: [9, 6]
+print(cem.filter_less_than(4))  # 4'ten küçük değerleri yazdırır: [3, 1, 1, 2, 3]
+print(cem.sum_values())  # Değerlerin toplamını yazdırır: 44
+print(cem.average())  # Değerlerin ortalamasını yazdırır: 4.0
